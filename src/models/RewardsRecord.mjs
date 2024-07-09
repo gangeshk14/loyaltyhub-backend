739bda68506d7954d499bd6a5a565a236fe0c49d
haresh02 <haresh.jayant@gmail.com>
2024-07-09 23:44:26 +0800

    updates

--- src/models/RewardsRecord.mjs
+++ src/models/RewardsRecord.mjs
@@ -4,32 +4,34 @@ import dbPool from "../config/database.mjs";
 class RewardsRecord{
     constructor(rewardsRecord) {
         this.recordID = rewardsRecord.recordID;
-        this.programID = rewardsRecord.programID;
-        this.userID = rewardsRecord.userID;
         this.date = rewardsRecord.date;
+        this.loyaltyProgramID = rewardsRecord.loyaltyProgramID;
+        this.userID = rewardsRecord.userID;
         this.points = rewardsRecord.points;
         this.rewardType = rewardsRecord.rewardType;
         this.rewardAmount = rewardsRecord.rewardAmount;
+        this.status = rewardsRecord.status || "PENDING";
         this.purpose = rewardsRecord.purpose;
-        this.status = rewardsRecord.status;
     }
 
     //Create record
-    static async create({userID, programID, date, points, rewardType, purpose}) {
+    static async create({userID, loyaltyProgramID, date, points, rewardType, status, purpose}) {
         const query = `
-            INSERT INTO RewardsRecord (UUID_TO_BIN(userID), UUID_TO_BIN(programID), date, points, rewardType, rewardAmount, purpose, status)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT INTO RewardsRecord (date, UUID_TO_BIN(loyaltyProgramID), UUID_TO_BIN(userID), points, rewardType, status, purpose)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
         `;
-        const values = [userID, programID, date, points, rewardType, rewardAmount, purpose, status];
+        const values = [date, loyaltyProgramID, userID, points, rewardType, status, purpose];
         try {
             const [result] = await dbPool.query(query, values);
             const recordID = result.insertId;
-            return new RewardsRecord({recordID, userID, programID, date, points, rewardType, rewardAmount, purpose, status});
+            return new RewardsRecord({recordID, date, loyaltyProgramID, userID, points, rewardType, status, purpose});
         } catch (err) {
             console.error('Error creating rewards record:', err);
             throw err;
         }
     }
+
+    // method here to update reward amount based on info
 
     //Find all records
 
