ba8bc9306ba0b532f46171183d37614beb559ea7
haresh02 <haresh.jayant@gmail.com>
2024-07-13 17:23:23 +0800

    fixed apis to be private

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -15,10 +15,10 @@ router.post('/api/login', loginUser);
 router.get('/api/users', getUsers);
 router.get('/profile', authMiddleware, getProfile);
 
-router.post('/api/rewardsrecords', createRewardRecord);
-router.get('/api/rewardsrecords/:recordID', getRewardRecordById);
-router.get('/api/rewardsrecords/user/:userID', getRewardRecordByUserID);
-router.put('/api/rewardsrecords/:recordID/status', updateRewardsRecordStatus);
+router.post('/api/rewardsrecords', authMiddleware, createRewardRecord);
+router.get('/api/rewardsrecords/:recordID', authMiddleware, getRewardRecordById);
+router.get('/api/rewardsrecords/user/:userID', authMiddleware, getRewardRecordByUserID);
+router.put('/api/rewardsrecords/:recordID/status', authMiddleware, updateRewardsRecordStatus);
 
 
 export default router;
